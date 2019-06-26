ae173e1e39f54d274cb976f97439e0c1a4604b3b
Zixin Huang <huang.zixin@mail.utoronto.ca>
2019-06-26 11:57:52 -0400

    small update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import NavBar from './components/layout/NavBar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar title="Github Finder" />
+        <NavBar />
+        <UserItem />
       </div>
     );
   }
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class UserItem extends Component {
+  render() {
+    return <div />;
+  }
+}
+
+export default UserItem;
